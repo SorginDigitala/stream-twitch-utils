3b2c9d3ed91f59c0690e0a9c6090e543be341c0e
aoeu <71111226+SorginDigitala@users.noreply.github.com>
2021-11-23 04:44:35 +0100

    html

--- assets/chat-alerts.js
+++ assets/chat-alerts.js
@@ -1,23 +1,22 @@
-const config={
+const config=localStorage.getItem("config_alerts")?JSON.parse(localStorage.getItem("config_alerts")):{
 	"channels"		:["seyacat","presuntamente"],
 	"mode"			:"alerts",
+	"volume"		:100,
 
 	"alerts"		:{
 		"sound_alert"	:"beep",
 		"custom_sound"	:"https://sorgindigitala.github.io/stream-twitch-utils/assets/audios/alerts/beep.mp3",
-		"volume"		:1,
 		"groups"		:[],
-		"ignores"		:[]
+		"users"			:[]
 	},
 
 	"tts"			:{
-		"volume"		:1,
 		"ignore_urls"	:true,		//ignora las url.
 		"short_urls"	:true,		//dice el nombre de dominio, no la url entera.
 		"rate_range"	:[.5,2],	//max rate allowed ( https://mdn.github.io/web-speech-api/speak-easy-synthesis/ )
 		"pitch_range"	:[0,2],		//max pitch allowed
 		"groups"		:[],
-		"ignores"		:[],
+		"users"			:["streamelements","nightbot"],
 	},
 
 	"chat"			:{
@@ -29,12 +28,19 @@ const config={
 		"reward_list"	:[],
 	},
 };
+/*
+Si se usa una versión vieja de la variable "config" podría dar error. Sería mejor usar una función que compare la config por defecto y la almacenada
+*/
 
 const groups=["broadcaster","vip","moderator","staff","admin","global_mod","viewer"];
 var audio_alert=new Audio("");
 var ws;
 
 
+
+function config_save(){
+	localStorage.setItem("config_alerts",JSON.stringify(config));
+}
 
 function load_config(){
 	document.body.onclick=e=>{panel.classList.toggle("hide",false)}
@@ -50,12 +56,14 @@ function load_config(){
 		config.channels=channels;
 		leave.forEach(c=>ws_leave(c));
 		join.forEach(c=>ws_join(c));
+		config_save();
 	}
 	mode_select.value=config.mode;
 	mode_select.onchange=e=>{
 		//desactivar el modo anterior si lo hubiera
 		//activar el nuevo modo si lo hubiera
 		//ocultar opciones de los demas modos
+		config_save();
 	}
 
 
@@ -66,9 +74,17 @@ function load_config(){
 		config.alerts.sound_alert=sound_alert.value;
 		audio_alert=new Audio(config.alerts.sound_alert==="custom"?config.alerts.custom_sound:"./assets/audios/alerts/"+config.alerts.sound_alert+".mp3");
 		play_alert();
+		config_save();
 	}
-	play_alert.onclick=play_alert;
+	alert_test.onclick=play_alert;
 	sound_alert.onchange();
+
+	config_volume.value=config.volume;
+	config_volume.onchange=e=>{
+		audio_alert.volume=config_volume.value/100;
+		config.volume=config_volume.value;
+		config_save();
+	}
 
 	alerts_groups.innerHTML=groups.map((e,i)=>"<label><input type=checkbox>"+e+"</label>").join("");
 	tts_groups.innerHTML=groups.map((e,i)=>"<label><input type=checkbox>"+e+"</label>").join("");
@@ -93,9 +109,11 @@ function start_ws(){
 		console.log("[irc-ws.chat] error:",e);
 	};
 	ws.onmessage=e=>{
-		if(e.data.trim().startsWith("PING"))
+		//hay que implementar sistema de log.
+		if(e.data.trim().startsWith("PING")){
+			debug_log.append("\n\rPing.");
 			ws.send("PONG");
-		else{
+		}else{
 			console.log(e.data);
 			//events.on_message_received(sender,msg);
 			audio_alert.play();
@@ -112,6 +130,7 @@ function ws_leave(e){
 
 
 function play_alert(){
+	audio_alert.volume=config.volume/100;
 	audio_alert.play().then().catch(e=>{
 		if(e.name!=="NotAllowedError")// ignorar error de permisos
 			console.error("error");
@@ -121,6 +140,12 @@ function play_alert(){
 function normalize_channel(c){
 	return c.trim().toLowerCase();
 }
+
+
+
+
+
+
 
 
 function Loader(){
